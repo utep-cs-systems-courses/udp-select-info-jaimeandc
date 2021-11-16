1ea0ee3df05939381be0129b7a1961bd1f4bc8ed
Andy <carlosajaime@Andys-MacBook-Pro.local>
2021-11-16 15:56:21 -0700

    Select working for both S/C still need to add dictionary and seg acknoledgment

--- UDP-server-select.py
+++ UDP-server-select.py
@@ -1,44 +1,73 @@
-#! /usr/bin/env python3
-# udp demo -- simple select-driven uppercase server
-
-# Eric Freudenthal with mods by Adrian Veliz
-
 import sys
 from socket import *
 from select import select
 
-upperServerAddr = ("", 50000)   # any addr, port 50,000
+upperServerAddr = ("", 50000)  # any addr, port 50,000
 
-def uppercase(sock):
-  "run this function when sock has rec'd a message"
-  message, clientAddrPort = sock.recvfrom(2048)
-  print("from %s: rec'd '%s'" % (repr(clientAddrPort), message))
-  modifiedMessage = message.decode().upper().encode()
-  sock.sendto(modifiedMessage, clientAddrPort)
-  
+def encapMessage(payload, segnum, msgPart): # Will add send/rec
+    segnum = segnum.to_bytes(2, sys.byteorder)#Save segment num
+    segnum = bytearray(segnum)
+    msgPart = bytearray(msgPart.encode())#Save if message is fi
+    paysize = len(payload) # get length of payload and save to
+    paysizebyte = paysize.to_bytes(2,sys.byteorder)
+    paysizebyte = bytearray(paysizebyte)
+    payload = bytearray(payload.encode()) #save payload in byte
+    packet = segnum + msgPart + paysizebyte + payload #Concat "
+    return packet
+def openPacket(packet):
+    packet = bytearray(packet)
+    segnum = packet[:2]
+    segnum = int.from_bytes(segnum,sys.byteorder)
+    msgPart = packet[2:3]
+    paysize = packet[3:5]
+    paysize = int.from_bytes(paysize,sys.byteorder)
+    payload = packet[-paysize:]
+    return segnum, msgPart.decode(), paysize, payload.decode()
+def getFile(sock):
+    "run this function when sock has rec'd a message"
+    message, clientAddrPort = sock.recvfrom(2048)
+    #print("from %s: rec'd '%s'" % (repr(clientAddrPort), message))
+    segnum, msgPart, paysize, message = openPacket(message)
+    currentfile = ""
+    if msgPart == "F": #If filename is sent
+        print("Received filename :", message)
+        currentfile = message
+        message = encapMessage("ready",segnum, "P")
+        sock.sendto(message, clientAddrPort)
+    elif msgPart == "P": #If file payload is sent
+        print("Receiving segment: ",segnum," - ", message)
+        #file = open(currentfile,"a")
+        message = encapMessage('ready',segnum,"P")
+        #file.close()
+        sock.sendto(message, clientAddrPort)
+    elif msgPart == "C":
+
+        print("Done sending file.")
+
+
+
 
 upperServerSocket = socket(AF_INET, SOCK_DGRAM)
 upperServerSocket.bind(upperServerAddr)
 upperServerSocket.setblocking(False)
 
 # map socket to function to call when socket is....
-readSockFunc = {}               # ready for reading
-writeSockFunc = {}              # ready for writing
-errorSockFunc = {}              # broken
+readSockFunc = {}  # ready for reading
+writeSockFunc = {}  # ready for writing
+errorSockFunc = {}  # broken
 
-timeout = 5                     # select delay before giving up, in seconds
+timeout = 5  # select delay before giving up, in seconds
 
 # function to call when upperServerSocket is ready for reading
-readSockFunc[upperServerSocket] = uppercase
+readSockFunc[upperServerSocket] = getFile
 
 print("ready to receive")
 while 1:
-  readRdySet, writeRdySet, errorRdySet = select(list(readSockFunc.keys()),
-                                                list(writeSockFunc.keys()), 
-                                                list(errorSockFunc.keys()),
-                                                timeout)
-  if not readRdySet and not writeRdySet and not errorRdySet:
-    print("timeout: no events")
-  for sock in readRdySet:
-    readSockFunc[sock](sock)
-
+    readRdySet, writeRdySet, errorRdySet = select(list(readSockFunc.keys()),
+                                                  list(writeSockFunc.keys()),
+                                                  list(errorSockFunc.keys()),
+                                                  timeout)
+    if not readRdySet and not writeRdySet and not errorRdySet:
+        print("timeout: no events.") ## NEED TO ADD TIMOUT FEATURE.
+    for sock in readRdySet:
+        readSockFunc[sock](sock)
--- clientFT.py
+++ clientFT.py
@@ -1,18 +1,111 @@
 #! /usr/bin/env python3
 from socket import *
-import sys
+import sys, os
 import select
+
 serverAddr = ('localhost', 50000)
 connection = socket(AF_INET, SOCK_DGRAM)
+connection.setblocking(False)
+filename = ""
+
+
+def read_in_chunks(file,size):
+    while True:
+        chunk = file.read(size)
+        if chunk:
+            yield chunk
+        else:
+            break
+def fileExist(filename):
+    file_exists = os.path.exists(filename)
+    if file_exists == True:
+        return True
+    else:
+        return False
+def getfileSize(file):
+    file_size = os.path.getsize(file)
+    file_size = file_size // 100
+    return file_size
+
+def readfile(file):
+    file=open(file,"rb")
+
+
+def encapMessage(payload, segnum, msgPart): # Will add send/recv & isfilename?
+    segnum = segnum.to_bytes(2, sys.byteorder)#Save segment num to byte array
+    segnum = bytearray(segnum)
+    msgPart = bytearray(msgPart.encode())#Save if message is filename message.
+    paysize = len(payload) # get length of payload and save to bytearray
+    paysizebyte = paysize.to_bytes(2,sys.byteorder)
+    paysizebyte = bytearray(paysizebyte)
+    payload = bytearray(payload.encode()) #save payload in bytearray
+    packet = segnum + msgPart + paysizebyte + payload #Concat "packet".
+    return packet
+def openPacket(packet):
+    packet = bytearray(packet)
+    segnum = packet[:2] #First 2 bytes are segnum
+    segnum = int.from_bytes(segnum,sys.byteorder)
+    msgPart = packet[2:3]  #Next byte is msgPart
+    paysize = packet[3:5]  #Last 2 bytes is length of payload.
+    paysize = int.from_bytes(paysize,sys.byteorder)
+    payload = packet[-paysize:]
+    return segnum, msgPart.decode(), paysize, payload.decode()
+
 
 while True:
-    readers, _, _ = select.select([connection,sys.stdin],[],[])
+    readers, writers, _ = select.select([connection,sys.stdin],[],[])
+
     for reader in readers:
         if reader is connection:
-            firstmsg, clientAddr = connection.recvfrom(1000)
-            print(firstmsg.decode())
+            ##WHAT DO DO ONCE MESSAGE HAS BEEN RECIVED.
+            firstmsg, clientAddr = connection.recvfrom(2048)
+            segnum, isfilename, paysize, message = openPacket(firstmsg) # SPLIT METADATA FROM PAYLOAD
+            print("MSG_FROM_SERVER:",message)
+            totalseg = getfileSize(filename)
+            #OPEN FILE AND START READING AND SENDING.
+            file = open(filename,"rb")
+            if message == "ready":
+                for currentRead in read_in_chunks(file,100):
+                    if segnum != totalseg+1:
+                        #currentRead = file.read(100)
+                        print(currentRead)
+                        print("currentseg:"+ str(segnum) + " totalseg:" + str(totalseg))
+                        packet = encapMessage(currentRead.decode(), segnum , "P")
+                        connection.sendto(packet,serverAddr)
+                        segnum += 1
+                else:
+                    print("DONE sending file.")
+                    sendCheck = encapMessage(str(totalseg+1).encode(),0, "C")
+                    file.close()
+                    exit(0)
+
+            else:
+                print("IDK WHAT YOUR ASKING FROM ME... YET")
+
+
+
+
+
+                    #packet = encapMessage(currentRead.decode(), currentseg, "N")
+                    #connection.sendto(packet,serverAddr)
+                #for currentRead in read_in_chunks(file,100):
+                #    print(currentRead.decode())
+                #    packet = encapMessage(currentRead.decode(), segnum, "N")
+                #    connection.sendto(packet,serverAddr)
+                #    segnum + 1
 
         else:
-            print("Input lowercase msg")
-            msg = sys.stdin.readline()[:-1]
-            connection.sendto(msg.encode(),serverAddr)
+            filename = sys.stdin.readline()[:-1]
+            print(getfileSize(filename))
+            if fileExist(filename) == True: #will change back to True later
+                msg = encapMessage(filename,0,"F")
+                connection.sendto(msg,serverAddr)
+
+
+            else:
+                print("Enter 'Real' File ")
+
+
+
+
+
